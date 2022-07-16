f9d5e4d2669868d220acd932316a731b4095710f
Nic Mortelliti <npmortelliti@gmail.com>
2022-07-16 18:55:05 +0000

    Add password to user table

--- db/migrate/20220716183114_create_users.rb
+++ db/migrate/20220716183114_create_users.rb
@@ -4,12 +4,14 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.string :username
+      t.string :password
     end
 
     User.create(
       first_name: 'Ruby',
       last_name: 'Stone',
-      username: 'admin'
+      username: 'admin',
+      password: 'password'
     )
   end
 end
