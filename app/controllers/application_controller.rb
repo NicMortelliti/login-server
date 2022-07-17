6a494bcdbea75b732e9b1c92a3959e577f6a4b94
NicMortelliti <npmortelliti@gmail.com>
2022-07-17 14:35:06 -0700

    Setup route for verifying user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,17 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(email: params[:email])
     user.to_json
   end
+
+  # Verify user password
+  get '/user/:email/:pass' do
+    user = User.find_by(email: params[:email])
+    if user.password === params[:pass]
+      user.name.to_json
+    else
+      status 403
+      body "Incorrect password.".to_json
+    end
+  end
   
   # Add new user
   post '/signup' do
