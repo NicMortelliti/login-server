6c780a5346b44cc995109b7fbd1b31696905b0ba
Nic Mortelliti <npmortelliti@gmail.com>
2022-07-17 01:06:32 +0000

    Create user signup control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
+  # Add new user
+  post '/signup' do
+    user = User.create(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    task.to_json(user)
+  end
 end
